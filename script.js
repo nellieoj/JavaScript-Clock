e8a9840cc2c10cdaffc8a12cdfdc1d817f0e0288
nellieoj <chineloleticia5@gmail.com>
2023-01-12 12:57:49 +0100

    Changes

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ window.onload = function () {
     let h = date.getHours() // 0 - 23
     let m = date.getMinutes() // 0 - 59
     let s = date.getSeconds() // 0 - 59
-    let session = 'AM'
+    let session = 'PM'
     if (h == 0) {
       h = 12
     }
     if (h > 12) {
       h = h - 12
-      session = 'PM'
+      session = 'AM'
     }
     h = h < 10 ? '0' + h : h
     m = m < 10 ? '0' + m : m
